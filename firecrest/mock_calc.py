bfafc142ec1f91fc3e79ec91d9874b8f8a1c684b
Chris Sewell <chrisj_sewell@hotmail.com>
2023-01-17 09:25:38 +0100

    Update mock_calc.py

--- firecrest/mock_calc.py
+++ firecrest/mock_calc.py
@@ -322,7 +322,7 @@ def main():
     logging.basicConfig(
         format="%(asctime)s:%(name)s:%(levelname)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.DEBUG,
+        level=logging.INFO,
     )
 
     computer = Computer(
